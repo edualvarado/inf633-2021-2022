d69127eee4b6c335a63d8247867234eed1acdb32
Eduardo Alvarado <33001422+edualvarado@users.noreply.github.com>
2021-11-21 13:12:41 +0100

    ADDED: Session 03

---
+++ Assets/02 - Scripts/03 - Character Animation/01 - Quadruped with full IK/FootStepper.cs
@@ -0,0 +1,203 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootStepper : MonoBehaviour
+{
+    [Header("Stepper Settings")]
+    public Transform homeTransform; // The position and rotation from which we want to stay in range (represented as the blue chip).
+    public float distanceThreshold = 0.4f; // If we exceed this distance threshold, we come back to the home position.
+    public float angleThreshold = 135f; // If we exceed this rotation threshold, we come back to the home rotation.
+    public float moveDuration; // The time it takes a step to complete.
+    [Range(0, 1)]
+    public float stepOvershootFraction; // To add some variability, we add a fraction to overshoot the target.
+
+    [Header("Raycast Settings")]
+    public LayerMask groundRaycastMask = ~0; // Ground layer that you need to detect by raycasting.
+    public float heightOffset; // Necessary when foot joints aren't exactly at the base of the foot geometry.
+
+    // Flag to define when a leg is moving.
+    public bool Moving;
+
+    // Awake is called when the script instance is being loaded.
+    void Awake()
+    {
+        // We put the steppers at the top of the hierarchy, to avoid other influences from the parent transforms and to see them better.
+        transform.SetParent(null);
+
+        // Adapt the legs just after starting the script.
+        MoveLeg();
+    }
+
+    /// <summary>
+    /// Method that set the preconditions for moving a leg and exectutes the coroutine process.
+    /// </summary>
+    public void MoveLeg()
+    {
+        // If we are already moving, don't start another move.
+        if (Moving)
+        {
+            return;
+        }
+
+        /*
+         * First, we want to calculate the distance from the GameObject where this script is attached (target, red sphere) to the home position of the respective leg (blue chip).
+         * We also calculate the quaternion between the current rotation and the home rotation.
+         * If such distance is larger than the threshold step, OR the angle difference is larger than the angle threshold, we call the coroutine to move the leg.
+         */
+
+        // START TODO ###################
+
+        // float distFromHome = ...
+        // float angleFromHome = ...
+
+        // Change condition!
+        if (false)
+        {
+            // END TODO ###################
+
+            // Get the grounded location for the feet. It can return false - in that case, it won't move.
+            // This method modifies the values by reference, which are used later.
+            if (GetGroundedEndPosition(out Vector3 endPos, out Vector3 endNormal))
+            {
+                // The foot rotation will be defined by:
+                // Forward direction: Forward normalized vector of home, but projected on the same plane where the grounded position was detected.
+                // Upward direction: Normal of the plane where the grounded position was detected.
+                Quaternion endRot = Quaternion.LookRotation(Vector3.ProjectOnPlane(homeTransform.forward, endNormal), endNormal);
+
+                // Start MoveFoot coroutine with the target position, rotation and duration of the movement.
+                StartCoroutine(MoveFoot(endPos, endRot, moveDuration));
+            }
+        }
+    }
+
+    /// <summary>
+    /// Method that saves the ground location information where the foot should be located.
+    /// Returns false if no grounded position was found.
+    /// </summary>
+    /// <param name="endPos"></param>
+    /// <param name="endNormal"></param>
+    /// <returns></returns>
+    bool GetGroundedEndPosition(out Vector3 endPos, out Vector3 endNormal)
+    {
+        /*
+         * First, we calculate the normalized vector that goes from the current position to the home position. It will describe the direction of our overshoot (offset) vector.
+         * Then, you can create a factor by taking the distance threshold and modifying it by a fraction [0, 1].
+         * Finally, you can multiply such new factor by the normalized distance previously calculated.
+         * The result is a vector in the direction of the movement when the foot is coming back to the home position, with a small magnitude.
+         * Summing such vector to the real home position, you will get a new home position slighly moved.
+         */
+
+        Vector3 towardsHome = (homeTransform.position - transform.position).normalized;
+        float overshootDistance = distanceThreshold * stepOvershootFraction;
+        Vector3 overshootVector = towardsHome * overshootDistance;
+
+        /*
+         * Now, we build a raycast system. Check: https://docs.unity3d.com/ScriptReference/Physics.Raycast.html
+         * The ray will detect a collision with the terrain and use such information to place the foot accordingly on the ground.
+         * First, you create the origin (Vector3) of your ray, which will be in the home position (remember to add the overshoot vector calculated before).
+         * You can also add some vertical y displacement to let the ray having more space when going down and avoiding undesired collisions.
+         * Then, throw the ray downwards, and save the position and normal vector of the hit in "endPos" and "endNormal" respectively.
+         * If there is a collision, return true. Otherwise, you can return false.
+         */
+
+        // START TODO ###################
+
+        // Vector3 raycastOrigin = ...
+
+        // if (Physics.Raycast(...))
+        // {
+        //  ...
+        //  return true;
+        // }
+
+        // END TODO ###################
+
+        endPos = Vector3.zero;
+        endNormal = Vector3.zero;
+        return false;
+    }
+
+    /// <summary>
+    /// Coroutine that performs the motion part.
+    /// </summary>
+    /// <param name="endPos"></param>
+    /// <param name="endRot"></param>
+    /// <param name="moveTime"></param>
+    /// <returns></returns>
+    IEnumerator MoveFoot(Vector3 endPos, Quaternion endRot, float moveTime)
+    {
+        // We are in the coroutine, meaning that a moving action is taking place.
+        Moving = true;
+
+        // Store the initial, current position and rotation for the interpolation.
+        Vector3 startPos = transform.position;
+        Quaternion startRot = transform.rotation;
+
+        // Apply the height offset to the home position, where we will move towards.
+        // You can change the heightOffset to see the effect. It just changes the vertical component to modify where the IK target is.
+        endPos += homeTransform.up * heightOffset;
+
+        // Initialize the time.
+        float timeElapsed = 0;
+
+        do
+        {
+            /*
+             * First, you need to keep the record of the total elapsed time.
+             * Then, you can normalized by using the moveTime variable.
+             */
+
+            timeElapsed += Time.deltaTime;
+            float normalizedTime = timeElapsed / moveTime;
+
+            // We could also apply some animation curve(e.g.Easing.EaseInOutCubic) to make the foot go smoother.
+            normalizedTime = Easing.EaseInOutCubic(normalizedTime);
+
+            /*
+             * We know startPos and endPos. We could interpolate directly from the starting point to the end point, but we have a problem: The movement would be straight and flat on the terrain. Try it out!
+             * transform.position = Vector3.Lerp(startPoint, endPoint, normalizedTime);
+             * We need to find a way to guide the foot from the ground to a lifted position, and then put it back on the ground. 
+             * Any idea? Just a tip: https://en.wikipedia.org/wiki/B%C3%A9zier_curve#Constructing_B.C3.A9zier_curves
+             */
+
+            // START TODO ###################
+
+            // transform.position = ...
+
+            // END TODO ###################
+
+            /*
+             * You just need now to also move from the starting rotation and the ending rotation.
+             */
+
+            // START TODO ###################
+
+            // transform.rotation = ...
+
+            // END TODO ###################
+
+            // Wait for one frame
+            yield return null;
+        }
+        while (timeElapsed < moveDuration);
+
+        // Motion has finished.
+        Moving = false;
+    }
+
+    /// <summary>
+    /// Function for better visualization.
+    /// </summary>
+    void OnDrawGizmosSelected()
+    {
+        if (Moving)
+            Gizmos.color = Color.green;
+        else
+            Gizmos.color = Color.red;
+
+        Gizmos.DrawWireSphere(transform.position, 0.25f);
+        Gizmos.DrawLine(transform.position, homeTransform.position);
+        Gizmos.DrawWireCube(homeTransform.position, Vector3.one * 0.1f);
+    }
+}
---
+++ Assets/02 - Scripts/03 - Character Animation/01 - Quadruped with full IK/QuadrupedProceduralMotion.cs
@@ -0,0 +1,291 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuadrupedProceduralMotion : MonoBehaviour
+{
+    [Header("Goal")]
+    public Transform goal; // The character will move towards this goal.
+
+    // Settings relative to the root motion.
+    [Header("Root Motion Settings")]
+    public float turnSpeed;
+    public float moveSpeed;
+    public float moveBackwardFactor = 0.5f;
+    [Space(20)]
+    public float turnAcceleration;
+    public float moveAcceleration;
+    [Space(20)]
+    public float minDistToGoal;
+    public float maxDistToGoal;
+    SmoothDamp.Vector3 currentVelocity;
+    SmoothDamp.Float currentAngularVelocity;
+
+    // Settings relative to body adaptation to the terrain.
+    [Header("Body Adaptation Settings")]
+    public Transform hips;
+    public float heightAcceleration;
+    public Vector3 constantHipsPosition;
+    public Vector3 constantHipsRotation;
+    public Transform groundChecker;
+    public Vector3 normalTerrain;
+    public float distanceHit;
+    public Vector3 posHit;
+
+    // Settings relative to the tail.
+    [Header("Tail Settings")]
+    public Transform[] tailBones;
+    public float tailTurnMultiplier;
+    public float tailTurnSpeed;
+    Quaternion[] tailHomeLocalRotation;
+    SmoothDamp.Float tailRotation;
+
+    // Settings relative to the head.
+    [Header("Head Settings")]
+    public Transform headBone; 
+    public float speedHead = 1f;
+    public bool headDebug = false;
+    public Vector3 goalWorldLookDir;
+    public Vector3 goalLocalLookDir;
+    public float angleHeadLimit = 90f;
+
+    // Foot Steppers for each leg.
+    [Header("Controllers for the steps")]
+    public FootStepper frontLeftFoot;
+    public FootStepper frontRightFoot;
+    public FootStepper backLeftFoot;
+    public FootStepper backRightFoot;
+
+    // Awake is called when the script instance is being loaded.
+    void Awake()
+    {
+        StartCoroutine(Gait());
+        TailInitialize();
+        BodyInitialize();
+    }
+
+    // Update is called every frame, if the MonoBehaviour is enabled.
+    private void Update()
+    {        
+        RootMotion();
+    }
+
+    // LateUpdate is called after all Update functions have been called.
+    private void LateUpdate()
+    {
+        TrackHead();
+        TailUpdate();
+        RootAdaptation();
+    }
+
+    #region Root Motion
+
+    /// <summary>
+    /// Method used to move the character towards the goal.
+    /// </summary>
+    private void RootMotion()
+    {
+        // Get the vector towards the goal and projectected it on the plane defined by the normal transform.up.
+        Vector3 towardGoal = goal.position - transform.position;
+        Vector3 towardGoalProjected = Vector3.ProjectOnPlane(towardGoal, transform.up);
+
+        // Angles between the forward direction and the direction to the goal. 
+        var angToGoal = Vector3.SignedAngle(transform.forward, towardGoalProjected, transform.up);
+
+        // Get a perfectange of the turnSpeed to apply based on how far the goal is and its sign.
+        float targetAngularVelocity = Mathf.Sign(angToGoal) * Mathf.InverseLerp(5f, 30f, Mathf.Abs(angToGoal)) * turnSpeed;
+
+        // Step() smoothing function.
+        currentAngularVelocity.Step(targetAngularVelocity, turnAcceleration);
+
+        // Initialize zero root velocity.
+        Vector3 targetVelocity = Vector3.zero;
+
+        // Estimating targetVelocity.
+        // Only translate if we are close facing to the goal.
+        if (Mathf.Abs(angToGoal) < 90)
+        {
+            var distToGoal = towardGoalProjected.magnitude;
+
+            // Move towards target if we are too far.
+            if (distToGoal > maxDistToGoal)
+            {
+                targetVelocity = moveSpeed * towardGoalProjected.normalized;
+            }
+            // Move away from target if we are too close.
+            else if (distToGoal < minDistToGoal)
+            {
+                targetVelocity = moveSpeed * -towardGoalProjected.normalized * moveBackwardFactor;
+            }
+
+            // Limit velocity progressively as we approach max angular velocity.
+            targetVelocity *= Mathf.InverseLerp(turnSpeed, turnSpeed * 0.2f, Mathf.Abs(currentAngularVelocity));
+        }
+
+        // Apply targetVelocity using Step() and applying.
+        currentVelocity.Step(targetVelocity, moveAcceleration);
+        transform.position += currentVelocity.currentValue * Time.deltaTime;
+
+        // Apply rotation.
+        transform.rotation *= Quaternion.AngleAxis(Time.deltaTime * currentAngularVelocity, transform.up);
+    }
+
+    #endregion
+
+    #region Root Adaptation
+
+    /// <summary>
+    /// Saves the initial position and rotation of the hips.
+    /// </summary>
+    private void BodyInitialize()
+    {
+        constantHipsPosition = new Vector3(hips.position.x, hips.position.y, hips.position.z);
+        constantHipsRotation = new Vector3(hips.rotation.x, hips.rotation.y, hips.rotation.z);
+    }
+
+    /// <summary>
+    /// In LateUpdate, after moving the root body to the target, we perform the adaptation on a top-layer to place the animal parallel to the ground.
+    /// </summary>
+    private void RootAdaptation()
+    {
+        // Origin of the ray.
+        Vector3 raycastOrigin = groundChecker.position;
+
+        // The ray information gives you where you hit and the normal of the terrain in that location.
+        if (Physics.Raycast(raycastOrigin, -transform.up, out RaycastHit hit, Mathf.Infinity))
+        {
+            if (hit.transform.gameObject.layer == LayerMask.NameToLayer("Ground"))
+            {
+                posHit = hit.point;
+                distanceHit = hit.distance;
+                normalTerrain = hit.normal;
+            }
+        }
+
+        /*
+         * In this layer, we need to refine the position and rotation of the hips based on the ground. Without this part, the animal would not lift its root body when walking on high terrains.
+         * First, try to use the hit information to modify hips.position and move it up when you are in a higher ground.
+         * Then, use also this information (normalTerrain) to rotate the root body and place it parallel to the ground. You can use Quaternion.FromToRotation() for that.
+         * When you have the angle that you want to have in your root body, you can place it directly, or use some interpolation technique to go smoothly to that value, in order to have less drastical movements.
+         */
+
+        // START TODO ###################
+
+        // hips.position = ...
+        // hips.rotation = ...
+
+        // END TODO ###################
+    }
+
+    #endregion
+
+    #region Tail Motion
+
+    /// <summary>
+    /// Initialize all the bones of the tail.
+    /// </summary>
+    void TailInitialize()
+    {
+        // Store the default rotation of the tail bones.
+        tailHomeLocalRotation = new Quaternion[tailBones.Length];
+        for (int i = 0; i < tailHomeLocalRotation.Length; i++)
+        {
+            tailHomeLocalRotation[i] = tailBones[i].localRotation;
+        }
+    }
+
+
+    /// <summary>
+    /// Rotate the tail as a function of the angular velocity of the root body.
+    /// </summary>
+    void TailUpdate()
+    {
+        // Tail rotates opposite to the current angular velocity to have some inertia effect.
+        tailRotation.Step(-currentAngularVelocity / turnSpeed * tailTurnMultiplier, tailTurnSpeed);
+
+        for (int i = 0; i < tailBones.Length; i++)
+        {
+            // Convert to Euler and apply the rotation my multiplying to the current local quaternion.
+            Quaternion rotation = Quaternion.Euler(0, tailRotation, 0);
+            tailBones[i].localRotation = rotation * tailHomeLocalRotation[i];
+        }
+    }
+
+    #endregion
+
+    #region Head Motion
+
+    /// <summary>
+    /// This method is used to rotate the head of your character in the direction of the goal.
+    /// </summary>
+    private void TrackHead()
+    {
+        // We save the local rotation of the head with respect to his parent, and reset it to the identity.
+        Quaternion currentLocalRotation = headBone.localRotation;
+        headBone.localRotation = Quaternion.identity;
+
+        /* 
+         * First, we need to get goalWorldLookDir: the position of the goal with respect to the head transform (you can use Debug.DrawRay() to debug it).
+         * Use InverseTransformDirection() and headbone.parent to transform it with respect to the parent of the head (goalLocalLookDir).
+         * Use RotateTowards() to have Vector3.forward always looking to goalLocalLookDir.
+         * Finally, define targetLocalRotation: The target local angle for your head. The forward axis (along the bone) will need to point to the object. To do this, you can use Quaternion.LookRotation().
+         */
+
+        // START TODO ###################
+
+        // goalWorldLookDir = ...
+        // goalLocalLookDir = ...
+
+        Quaternion targetLocalRotation = Quaternion.identity; // Change!
+
+        // END TODO ###################
+
+        /* 
+         * Interpolation for damping. 
+         * We do not apply directly the quaternion to the head (that would cause a quite robotic movement).
+         * Instead, we just use interpolation to apply it over time, to provide a more natural effect.
+         */
+
+        headBone.localRotation = Quaternion.Slerp(currentLocalRotation, targetLocalRotation, 1 - Mathf.Exp(-speedHead * Time.deltaTime));
+    }
+
+    #endregion
+
+    #region Gait
+
+    /// <summary>
+    /// Coroutine that describes how the gait of the character will be.
+    /// It calls the MoveLeg() function for each leg in a defined moment. In this case, we want the diagonal legs pair move simultaneiously, while the other pair of diagonal legs stays in place.
+    /// This is necessary as we do not have any kinematic animation - our character moves purely with IK and procedural functions.
+    /// Other complex behaviors might be created.
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator Gait()
+    {
+        while (true)
+        {
+            do
+            {
+                frontLeftFoot.MoveLeg();
+                backRightFoot.MoveLeg();
+
+                // Wait a frame
+                yield return null;
+
+            } while (backRightFoot.Moving || frontLeftFoot.Moving);
+
+            // Do the same thing for the other diagonal pair
+            do
+            {
+                frontRightFoot.MoveLeg();
+                backLeftFoot.MoveLeg();
+
+                // Wait a frame
+                yield return null;
+
+            } while (backLeftFoot.Moving || frontRightFoot.Moving);
+        }
+    }
+
+    #endregion
+}
